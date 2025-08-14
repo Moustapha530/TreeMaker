7f55f9fe2ccaed258f5136cf2dd7455ba04c64de
Moustapha Mukwini <moustaphamukwini@gmail.com>
2025-08-14 15:44:43 +0100

    feat (utils) : remake strndup function

---
+++ includes/utils.h
@@ -0,0 +1,9 @@
+#ifndef __UTILS_H__
+    #define __UTILS_H__
+
+    #include <string.h>
+    #include <stdlib.h>
+
+    char *_strndup(const char *src, size_t n);
+    
+#endif
--- src/utils.c
+++ src/utils.c
@@ -1,1 +1,14 @@
 #include "utils.h"
+
+char *_strndup(const char *src, size_t n){
+    if(src == NULL)
+        return NULL;
+
+    size_t len = strnlen(src, n);
+    char *dest = malloc(len + 1);
+    if(dest == NULL)
+        return NULL;
+    memcpy(dest, src, len);
+    dest[len] = '\0';
+    return dest;
+}
