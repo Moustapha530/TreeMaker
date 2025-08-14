5ae0e009de59b77ffa3a6439b00b3b1daf45a991
Moustapha Mukwini <moustaphamukwini@gmail.com>
2025-08-14 14:21:10 +0100

    feat (main) : create the base main fonction for the app

--- src/main.c
+++ src/main.c
@@ -1,9 +1,23 @@
 /*
     TreeMaker console application to create tree structure from a trm file. 
 */ 
-#include <stdio.h>
+#include "args.h"
+#include "parser.h"
 
-int main(int argc, char *argv[]){
-    printf("Hello word !");
+int main(int argc, char **argv){
+    Args args;
+    parse_args(argc, argv, &args);
+
+    for(size_t i = 0; i < args.file_count; i++){
+        Tree tr = parse_file(args.input_files[i]);
+        if(!tr){
+            fprintf(stderr, "fatal : parsing error please check the input file \"%s\".\n", args.input_files[i]);
+            exit(EXIT_FAILURE);
+        } else {
+            print_tree(tr, 0);
+            clean_tree(&tr);
+        }
+    }
+    free_args(&args);
     return 0;
 }
