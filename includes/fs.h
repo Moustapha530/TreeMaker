5c6ffd8c6e3824c3360a537504078917fba41091
Moustapha Mukwini <moustaphamukwini@gmail.com>
2025-08-15 22:12:47 +0100

    feat (fs) : print happened errors

--- includes/fs.h
+++ includes/fs.h
@@ -1,7 +1,25 @@
 #ifndef __FS_H__
-#define __FS_H__
+    #define __FS_H__
 
-int create_file(const char *path);
-int create_folder(const char *path);
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <stdbool.h>
+
+    #ifdef _WIN32
+        #include <direct.h> 
+        #include <windows.h>
+        #include <io.h>
+        #define PATH_SEPARATOR '\\'
+    #else
+        #include <sys/types.h>
+        #include <sys/stat.h>
+        #include <fcntl.h>
+        #include <unistd.h>
+        #define PATH_SEPARATOR '/'
+    #endif
+
+    int create_file(const char *path);
+    int create_folder(const char *path);
 
 #endif
--- src/fs.c
+++ src/fs.c
@@ -1,36 +1,19 @@
 #include "fs.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#ifdef _WIN32
-    #include <direct.h>  // mkdir on Windows
-    #include <windows.h>
-    #include <io.h>
-    #define PATH_SEPARATOR '\\'
-#else
-    #include <sys/types.h>
-    #include <sys/stat.h>
-    #include <fcntl.h>
-    #include <unistd.h>
-    #define PATH_SEPARATOR '/'
-#endif
-
 int create_folder(const char *path){
     #ifdef _WIN32
-        if(_mkdir(path) == 0 || errno == EEXIST){
-            return 0;
-        } else {
-            perror("mkdir(Windows)");
-            return -1;
+        if(_mkdir(path) == 0 || errno == EEXIST)
+            return EXIT_SUCCESS;
+        else{
+            fprintf(stderr, "error : failed to create directory \"%s\".\n", path);
+            return EXIT_FAILURE;
         }
     #else
-        if(mkdir(path, 0755) == 0 || errno == EEXIST){
-            return 0;
-        } else {
-            perror("mkdir(Unix)");
-            return -1;
+        if(mkdir(path, 0755) == 0 || errno == EEXIST)
+            return EXIT_SUCCESS;
+        else{
+            fprintf(stderr, "error : failed to create file \"%s\".\n", path);
+            return EXIT_FAILURE;
         }
     #endif
 }
@@ -39,18 +22,18 @@ int create_file(const char *path){
     #ifdef _WIN32
         FILE *f = fopen(path, "w");
         if(f == NULL){
-            perror("fopen(Windows)");
-            return -1;
+            fprintf(stderr, "error : failed to create file \"%s\".\n", path);
+            return EXIT_FAILURE;
         }
         fclose(f);
-        return 0;
+        return EXIT_SUCCESS;
     #else
         int fd = open(path, O_CREAT | O_WRONLY, 0644);
         if(fd < 0){
-            perror("open(Unix)");
-            return -1;
+            fprintf(stderr, "error : failed to create file \"%s\".\n", path);
+            return EXIT_FAILURE;
         }
         close(fd);
-        return 0;
+        return EXIT_SUCCESS;
     #endif
 }
